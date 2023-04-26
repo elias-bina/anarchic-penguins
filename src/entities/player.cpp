c00fab7706ee6d020dee6a2a12224b199e1da722
Elias Bina <elias.bina58@gmail.com>
2023-04-26 18:05:51 +0200

    feat: Add time management for the player displacement

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -1,50 +1,63 @@
-
 #include "entities/player.h"
 #include "constants.h"
 
 #include <iostream>
+
+// Speed constants : Pixel per seconds
+constexpr float MAX_PLAYER_SPEED = 200.0f;
+constexpr int32_t NB_MILLISECONDS_RANDOM_CONTINUES = 50;
 
 Player::Player() : Entity() {}
 
 Player::~Player() {}
 
 void Player::Update(std::chrono::nanoseconds duration) {
-  move_at_random();
-  constexpr std::chrono::nanoseconds ms = std::chrono::milliseconds(1);
-  std::cout << "Time elapsed : " << duration / ms << "ms" << std::endl;
+  move_at_random(duration);
+  constexpr std::chrono::nanoseconds µs = std::chrono::microseconds(1);
+  std::cout << "Time elapsed : " << duration / µs << "µs" << std::endl;
   return;
 }
 
-void Player::move_x(float pos) {
+void Player::move_x_pixels(float pos) {
   this->setPosition(this->getPosition() + sf::Vector2f(pos, 0.0f));
 }
-void Player::move_y(float pos) {
+void Player::move_y_pixels(float pos) {
   this->setPosition(this->getPosition() + sf::Vector2f(0.0f, pos));
 }
 
-void Player::move_at_random() {
+void Player::move_at_random(std::chrono::nanoseconds duration) {
+  static std::chrono::time_point<std::chrono::steady_clock> last_move_choice =
+      std::chrono::steady_clock::now();
+
+  static int choice = 0;
+
+  float displacement = MAX_PLAYER_SPEED * duration / std::chrono::seconds(1);
   if (!_has_set_seed) {
     std::srand(std::time(nullptr));
     _has_set_seed = true;
   }
-  for (uint32_t i = 0; i < 10; i++) {
-    int choice = std::rand() % 4;
-    switch (choice) {
-    case 0:
-      move_x(1);
-      break;
-    case 1:
-      move_x(-1);
-      break;
-    case 2:
-      move_y(1);
-      break;
-    case 3:
-      move_y(-1);
-      break;
 
-    default:
-      break;
-    }
+  if (std::chrono::duration_cast<std::chrono::nanoseconds>(
+          std::chrono::steady_clock::now() - last_move_choice) >
+      std::chrono::milliseconds(NB_MILLISECONDS_RANDOM_CONTINUES)) {
+    choice = std::rand() % 4;
+    last_move_choice = std::chrono::steady_clock::now();
+  }
+  switch (choice) {
+  case 0:
+    move_x_pixels(displacement);
+    break;
+  case 1:
+    move_x_pixels(-displacement);
+    break;
+  case 2:
+    move_y_pixels(displacement);
+    break;
+  case 3:
+    move_y_pixels(-displacement);
+    break;
+
+  default:
+    break;
   }
 }
