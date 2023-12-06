682e96aabe57c41558b992a3c085b5be59426bc4
Elias Bina <elias.bina58@gmail.com>
2023-12-06 09:40:58 +0100

    feat: Input controller get values

---
+++ tests/input/input-controller-tests.cpp
@@ -0,0 +1,28 @@
+#include <criterion/criterion.h>
+
+#include "input/input-controller.h"
+
+Test(input_controller, default_state) {
+  InputController controller;
+
+  cr_assert(controller.get_joystick_index() == UNDEFINED_INDEX);
+  cr_assert(!controller.has_joystick());
+  cr_assert(!controller.has_player());
+  cr_assert(!controller.has_keyboard());
+  cr_assert(controller.get_vector_move() == sf::Vector2f(0.0f, 0.0f));
+  cr_assert(controller.get_vector_camera() == sf::Vector2f(0.0f, 0.0f));
+}
+
+Test(input_controller, set_values) {
+  InputController controller;
+
+  cr_assert(0);
+  // cr_assert(controller.get_joystick_index() == UNDEFINED_INDEX);
+  // cr_assert(!controller.has_joystick());
+  // cr_assert(!controller.has_player());
+  // cr_assert(!controller.has_keyboard());
+  // cr_assert(controller.get_vector_move() == sf::Vector2f(0.0f, 0.0f));
+}
+
+// TODO: Check that keyboard_input fails if no keyboard
+//        same for joystick
--- tests/input/project.mk
+++ tests/input/project.mk
@@ -2,10 +2,11 @@ include src/input/project.mk
 
 TEST_INPUT_EXE=$(BIN)/tests-input-make-exe
 
-TEST_INPUT_SRCS:=				\
-	$(INPUT-SRCS)				\
-	tests/input/action_list.cpp	\
-	tests/input/tests_input.cpp
+TEST_INPUT_SRCS:=							\
+	$(INPUT-SRCS)							\
+	tests/input/action-list-tests.cpp		\
+	tests/input/input-controller-tests.cpp	\
+	tests/input/tests-input.cpp
 
 
 
